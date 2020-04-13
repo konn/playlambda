778fa561c5640c7d3daaa29d8b6667f22a8ffbfe
Hiromi ISHII <konn.jinro@gmail.com>
2020-04-13 21:09:38 +0900

    common compiles

--- common/src/Web/PlayAtHome/Route.hs
+++ common/src/Web/PlayAtHome/Route.hs
@@ -17,19 +17,19 @@ import Obelisk.Route.TH
 data BackendRoute :: * -> * where
   -- | Used to handle unparseable routes.
   BackendRoute_Missing :: BackendRoute ()
-  -- You can define any routes that will be handled specially by the backend here.
-  -- i.e. These do not serve the frontend, but do something different, such as serving static files.
+  BackendRoute_Room    :: BackendRoute ()
 
 data FrontendRoute :: * -> * where
   FrontendRoute_Main :: FrontendRoute ()
-  -- This type is used to define frontend routes, i.e. ones for which the backend will serve the frontend.
 
 fullRouteEncoder
   :: Encoder (Either Text) Identity (R (FullRoute BackendRoute FrontendRoute)) PageName
 fullRouteEncoder = mkFullRouteEncoder
   (FullRoute_Backend BackendRoute_Missing :/ ())
   (\case
-      BackendRoute_Missing -> PathSegment "missing" $ unitEncoder mempty)
+      BackendRoute_Missing -> PathSegment "missing" $ unitEncoder mempty
+      BackendRoute_Room -> PathSegment "room" $ unitEncoder mempty
+  )
   (\case
       FrontendRoute_Main -> PathEnd $ unitEncoder mempty)
 
